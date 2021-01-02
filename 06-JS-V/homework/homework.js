a4754314d518bb190b3cf97d376b7c300e63dcaf
luciaangrigiani <lucia.angrigiani@gmail.com>
2021-01-01 23:49:45 -0300

    Termina parte JS

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,6 +8,16 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario (opciones) {
+    this.usuario = opciones.usuario 
+    this.nombre = opciones.nombre 
+    this.email = opciones.email 
+    this.password = opciones.password 
+  }
+  Usuario.prototype.saludar = function() {
+    return "Hola, mi nombre es " + this.nombre 
+  }
+  return Usuario
 
 }
 
@@ -15,7 +25,9 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-
+  Constructor.prototype.saludar = function() {
+    return "Hello World!"
+  }
 }
 
 function agregarStringInvertida() {
@@ -24,6 +36,14 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+  String.prototype.reverse = function() {
+    var array = this.split("")
+    var newarray = []
+   for (i=0; i<array.length; i++) {
+      newarray.push (array [array.length-i-1])
+    }
+    return newarray.join("")
+  }
 
 }
 
